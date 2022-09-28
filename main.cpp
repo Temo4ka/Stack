6989ca39380a40fffb9d0be08d393b10920c7c3a
Temo4ka <67861483+Temo4ka@users.noreply.github.com>
2022-09-28 20:52:16 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#ifndef Elem_t
+    #include "stackType.h"
+#endif
+#include "stack.h"
+#include <cstdio>
+#include <string.h>
+
+void userPrint(FILE *stream, const Elem_t Param) {
+    fprintf(stream, "%d", Param);
+    fprintf(stderr, "%d", Param);
+}
+
+int main() {
+
+    Stack stack = {};
+
+    stackCtor(&stack, userPrint)
+
+    int er = 0;
+
+    for (int i = 0; i < 10; i++) {
+        int k = 0;
+        scanf("%d", &k);
+        if (k == 0) {
+            printf("%d\n", stackPop(&stack, &er));
+        } else
+            stackPush(&stack, k);
+        stackDump(&stack);
+    }
+
+    stackDtor(&stack);
+    logClose();
+    return 0;
+}
